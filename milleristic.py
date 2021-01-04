c469df4868a03643cc74c337e5f2348d4ba233ce
RuwaidMuntajim <68751420+RuwaidMuntajim@users.noreply.github.com>
2021-01-04 22:30:14 +0600

    Create milleristic.py

---
+++ milleristic.py
@@ -0,0 +1,6 @@
+vid = "fgbrrgthrfhtrfhgtrfrthtrhthrthttrhtrgrgferhtrhghtrbrthtrhtrhrrtrrgrg"
+vid1 = "TRgrgrgr"
+bitch = "samule the bitchless"
+
+ddfffffffffffffffffffffffffffff
+print("Hello world")
